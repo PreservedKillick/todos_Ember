567637ff6970e28070ca07d4635e7a77a6baf5f8
Angela Yen <ayen10@gmail.com>
2014-04-28 17:22:45 -0700

    have readme and functionality to add new task and mark tasks as complete (or not)

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -1,8 +1,9 @@
 Todos.TodoController = Ember.ObjectController.extend({
   isCompleted: function(key, value){
     var model = this.get('model');
-
+      console.debug(value);
     if (value === undefined) {
+
       return model.get('isCompleted');
     } else {
       model.set('isCompleted', value);
